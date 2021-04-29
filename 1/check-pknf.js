ba49c82c730dfd0a89b2da8ad9f335d41b440def
Alexey NIkipelau <alexey.nikipelau@form.io>
2021-04-29 16:47:50 +0300

    files updated

--- 1/check-pknf.js
+++ 1/check-pknf.js
@@ -23,7 +23,7 @@ const checkPknf = str => {
     syntaxCheck(tokens)
     tokens.forEach(t => parseToken(t))
     const parseTree = getParseTree()
-
+    let parentDisjunction;
     try {
         preOrderTraversal(parseTree, (node, parent) => { // meta says about parent node operator: true if OR, false if smth else
             if (node?.operator?.arity === ARITY.BINARY) {
@@ -35,24 +35,31 @@ const checkPknf = str => {
                 if (parent?.operator?.type === TOKENS.OR && node.operator.type !== TOKENS.OR) {
                     throw new NotKnfError('There should be only disjunctions after disjunctions')
                 }
+                if (parent?.operator?.type === TOKENS.AND && node.operator.type !== TOKENS.OR) {
+                    parentDisjunction = node;
+                    if(!parentDisjunction.vars) parentDisjunction.vars = [];
+                }
+                if(node.operator.type === TOKENS.OR) {
+                    node.childs[0].type === 'var' && parentDisjunction.vars.push(node.childs[0])
+                    node.childs[1].type === 'var' && parentDisjunction.vars.push(node.childs[1])
+                }
             } else if (node?.operator?.arity === ARITY.UNARY) {
                 if(node?.childs[0].type === 'formula') throw new NotKnfError('Negotiation to subformulas is not allowed')
-                if (!parent?.vars) {
-                    parent.vars = []
+                if (!parentDisjunction?.vars) {
+                    parentDisjunction.vars = []
                 }
-                parent.vars.push({
+                parentDisjunction.vars.push({
                     type: node.childs[0].type,
                     value: node.childs[0].value,
                     neg: true,
                 })
             } else if (node?.type === 'var' && parent?.type !== TOKENS.NOT) {
-                if (!parent?.vars) {
-                    parent.vars = []
+                if (!parentDisjunction?.vars) {
+                    parentDisjunction.vars = []
                 }
-                parent.vars.push({
+                parentDisjunction.vars.push({
                     type: node.type,
                     value: node.value,
-
                 })
             }
         })
