1143c53e4d23f01ba5425de7fd4abf2b514a01ff
Evencaster <iloveyoucreator@gmail.com>
2021-05-02 14:51:16 +0300

    input changed

--- 1/main.js
+++ 1/main.js
@@ -3,7 +3,7 @@
 const readline = require('readline-sync')
 const checkPknf = require('./check-pknf')
 
-// const str = readline.question('Enter formula:\n')
-const str = '(A/\\(B/\\(C\\/(!D))))'
+const str = readline.question('Enter formula:\n')
+// const str = '(A/\\(B/\\(C\\/(!D))))'
 
 console.log(checkPknf(str))
