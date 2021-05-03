a91a393ed9d200f793c7b7d5a2cbdca251caa024
Alexey NIkipelau <iloveyoucreator@gmail.com>
2021-05-03 11:24:16 +0300

    consts fixed

--- parser.js
+++ parser.js
@@ -30,7 +30,7 @@ const Parser = () => {
         } else if (VARS.includes(token.type)) {
             if(!pointer.childs) pointer.childs = []
             if(pointer.childs.length === 2) throw new UnexpectedVariableError(token)
-            pointer.childs.push({type: 'var', value: token.value})
+            pointer.childs.push({type: 'var', value: token.value, const: token.type === TOKENS.CONST})
 
         } else if (OPERATORS.includes(token.type)) {
             if (pointer.operator) throw new UnexpectedOperatorError(token)
